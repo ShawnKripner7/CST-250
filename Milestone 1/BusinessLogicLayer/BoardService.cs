50cc1c82c8b43d033c6d8c03e3cbf61d9a31a919
ShawnKripner7 <shawn.kripner7@gmail.com>
2026-03-26 19:43:38 -0700

    Add files via upload

---
+++ Milestone 1/BusinessLogicLayer/BoardService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using MinesweeperClassLibrary.Models;
+
+namespace MinesweeperClassLibrary.BusinessLogicLayer
+{
+    public class BoardService
+    {
+        private BoardModel _board;
+        private Random _random = new Random();
+
+        // constructor gets the board we are working with
+        public BoardService(BoardModel board)
+        {
+            _board = board;
+        }
+
+        // randomly place bombs on the board
+        public void SetupBombs()
+        {
+            int bombCount = _board.Size;
+            int bombsPlaced = 0;
+
+            while (bombsPlaced < bombCount)
+            {
+                int row = _random.Next(_board.Size);
+                int col = _random.Next(_board.Size);
+
+                if (_board.Cells[row, col].IsBomb == false)
+                {
+                    _board.Cells[row, col].IsBomb = true;
+                    bombsPlaced++;
+                }
+            }
+        }
+
+        // count how many bombs are around each cell
+        public void CountBombsNearby()
+        {
+            for (int row = 0; row < _board.Size; row++)
+            {
+                for (int col = 0; col < _board.Size; col++)
+                {
+                    int bombCount = 0;
+
+                    // check all 8 directions around the cell
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int neighborRow = row + i;
+                            int neighborCol = col + j;
+
+                            // make sure we stay inside the board
+                            if (neighborRow >= 0 && neighborRow < _board.Size &&
+                                neighborCol >= 0 && neighborCol < _board.Size)
+                            {
+                                if (_board.Cells[neighborRow, neighborCol].IsBomb)
+                                {
+                                    bombCount++;
+                                }
+                            }
+                        }
+                    }
+
+                    // save the count to the cell
+                    _board.Cells[row, col].NumberOfBombNeighbors = bombCount;
+                }
+            }
+        }
+    }
+}
---
+++ Milestone 1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using MinesweeperClassLibrary.Models;
+using MinesweeperClassLibrary.BusinessLogicLayer;
+
+namespace MinesweeperConsoleApp
+{
+    // entry point of the program
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a 15x15 board
+            BoardModel board = new BoardModel(15);
+
+            // create service to handle game logic
+            BoardService service = new BoardService(board);
+
+            // set up bombs on the board
+            service.SetupBombs();
+
+            // calculate number of bombs around each cell
+            service.CountBombsNearby();
+
+            // print the board to the console
+            PrintAnswers(board);
+
+            Console.ReadLine();
+        }
+
+        // prints the board to the console
+        static void PrintAnswers(BoardModel board)
+        {
+            // loop through each row
+            for (int row = 0; row < board.Size; row++)
+            {
+                // loop through each column
+                for (int col = 0; col < board.Size; col++)
+                {
+                    // if the cell is a bomb
+                    if (board.Cells[row, col].IsBomb)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("B ");
+                    }
+                    // if the cell has bombs nearby
+                    else if (board.Cells[row, col].NumberOfBombNeighbors > 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(board.Cells[row, col].NumberOfBombNeighbors + " ");
+                    }
+                    // if the cell is empty
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write(". ");
+                    }
+                }
+
+                // move to next row
+                Console.WriteLine();
+            }
+
+            // reset color back to normal
+            Console.ResetColor();
+        }
+    }
+}
