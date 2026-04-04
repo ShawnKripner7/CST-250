883a85928c9a1ac88bc148c0c7c805f3b66b6e79
ShawnKripner7 <shawn.kripner7@gmail.com>
2026-04-04 02:00:58 -0700

    Finished the UI setup for the GUI chessboard

---
+++ Activity 2/FrmChessBoard.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ChessBoardGUIApp
+{
+    partial class FrmChessBoard
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            cmbChesPieces = new Panel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(34, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(394, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Select a chess piece and its location on the board and see the legal moves";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(704, 34);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Pieces:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "King", "Queen", "Bishop", "Knight", "Rook" });
+            comboBox1.Location = new Point(763, 26);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // cmbChesPieces
+            // 
+            cmbChesPieces.Location = new Point(34, 64);
+            cmbChesPieces.Name = "cmbChesPieces";
+            cmbChesPieces.Size = new Size(500, 500);
+            cmbChesPieces.TabIndex = 3;
+            // 
+            // FrmChessBoard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(998, 980);
+            Controls.Add(cmbChesPieces);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FrmChessBoard";
+            Text = "Chess Board";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Panel cmbChesPieces;
+    }
+}
---
+++ Activity 2/FrmChessBoard.cs
@@ -0,0 +1,10 @@
+namespace ChessBoardGUIApp
+{
+    public partial class FrmChessBoard : Form
+    {
+        public FrmChessBoard()
+        {
+            InitializeComponent();
+        }
+    }
+}
