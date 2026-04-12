4895ff5d16792659323d33030a84a9f3fd785c29
ShawnKripner7 <shawn.kripner7@gmail.com>
2026-04-11 20:45:47 -0700

    Add a FloodFill method to perform the flood fill algorithm on a board

--- Activity 3/FloodFillRecursion/FloodFillRecursion/FloodFillRecursion/Program.cs
+++ Activity 3/FloodFillRecursion/FloodFillRecursion/FloodFillRecursion/Program.cs
@@ -18,6 +18,12 @@ BoardModel board = new BoardModel(20, 3);
 // Print the board to the console
 Utility.PrintBoard(board);
 
+// Call the flood fill method using the board
+board = Utility.FloodFill(board, 0, 0);
+
+// Print the new board
+Utility.PrintBoard(board);
+
 //------------------------------------------------------------
 // End of the Main Method
 //------------------------------------------------------------
@@ -48,7 +54,7 @@ static class Utility
         // Loop through the rows of the board
         for (int row = 0; row < board.Size; row++)
         {
-            // Print each row number in white
+            // Print each row number
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write($"{row + 1,2}");
 
@@ -66,13 +72,114 @@ static class Utility
                     Console.ForegroundColor = ConsoleColor.White;
                     Console.Write(" . ");
                 }
+                else if (board.Grid[row, col].Contents == "F")
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.Write(" ~ ");
+                }
                 else
                 {
                     Console.Write("   ");
                 }
             }
 
+            // Use a WriteLine to start a new row
             Console.WriteLine();
         }
     } // End of PrintBoard method
+
+    /// <summary>
+    /// Perform a flood fill algorithm on the given row and col
+    /// </summary>
+    /// <param name="board"></param>
+    /// <returns></returns>
+    internal static BoardModel FloodFill(BoardModel board, int row, int col)
+    {
+        // Declare and initialize
+        int sleepCount = 25; // milliseconds
+
+        // Change the text color to white
+        Console.ForegroundColor = ConsoleColor.White;
+
+        // Print the current cell to the console
+        Console.Write($"Filling at {row}, {col} ");
+
+        // Pause the program for sleepCount number of milliseconds
+        Thread.Sleep(sleepCount);
+
+        // Check if the cell is on the board
+        if (row < 0 || row >= board.Size || col < 0 || col >= board.Size)
+        {
+            // Print a message indicating the cell is out of bounds
+            Console.WriteLine("Out of bounds. Stop");
+            // Pause the program for sleepCount number of milliseconds
+            Thread.Sleep(sleepCount);
+
+            // If the cell is not on the board, end the method
+            return board;
+        }
+
+        // If the cell is a wall, end the method
+        if (board.Grid[row, col].Contents == "W")
+        {
+            // Print a message indicating the cell is a wall
+            Console.WriteLine("Hit a wall. Stop");
+            // Pause the program for sleepCount number of milliseconds
+            Thread.Sleep(sleepCount);
+
+            return board;
+        }
+        // If the cell has already been filled, end the method
+        else if (board.Grid[row, col].Contents == "F")
+        {
+            // Print a message indicating the cell already filled
+            Console.WriteLine("Already Filled. Stop");
+            // Pause the program for sleepCount number of milliseconds
+            Thread.Sleep(sleepCount);
+
+            return board;
+        }
+
+        // Else, fill the cell
+        else
+        {
+            board.Grid[row, col].Contents = "F";
+            // Pause the program for sleepCount number of milliseconds
+            Thread.Sleep(sleepCount);
+        }
+
+        // Improve the visual effect of the flood fill
+        // Comment out to have program history
+        Console.Clear();
+        // Print the current board
+        Console.WriteLine();
+        PrintBoard(board);
+
+        // Print a message indicating the next flood fill direction
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("North: ");
+        // Call the flood fill method to the north
+        board = FloodFill(board, row - 1, col);
+
+        // Print a message indicating the next flood fill direction
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("East: ");
+        // Call the flood fill method to the east
+        board = FloodFill(board, row, col + 1);
+
+        // Print a message indicating the next flood fill direction
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Write("South: ");
+        // Call the flood fill method to the south
+        board = FloodFill(board, row + 1, col);
+
+        // Print a message indicating the next flood fill direction
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write("West: ");
+        // Call the flood fill method to the west
+        board = FloodFill(board, row, col - 1);
+
+        // Return the board
+        return board;
+    } // End of FloodFill method
 }
