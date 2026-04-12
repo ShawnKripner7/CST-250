a10e5cb6368d52b5b44e25a26e435c84a401a714
ShawnKripner7 <shawn.kripner7@gmail.com>
2026-04-11 20:23:43 -0700

    Add a Utility class with a PrintBoard method to display the board to the user

---
+++ Activity 3/FloodFillRecursion/FloodFillRecursion/FloodFillRecursion/Models/CellModel.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Shawn Kripner
+ * CST - 250
+ * 4/10/2026
+ * Flood Fill Recursion
+ * Activity 3
+ */
+
+namespace FloodFillRecursion.Models
+{
+    internal class CellModel
+    {
+        // Cell Model Properties
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public string Contents { get; set; }
+
+        /// <summary>
+        /// Parameterized constructor for CellModel
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        /// <param name="contents"></param>
+        public CellModel(int row, int column, string contents)
+        {
+            Row = row;
+            Column = column;
+            Contents = contents;
+        }
+    }
+}
---
+++ Activity 3/FloodFillRecursion/FloodFillRecursion/FloodFillRecursion/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Shawn Kripner
+ * CST - 250
+ * 4/10/2026
+ * Flood Fill Recursion
+ * Activity 3
+ */
+using FloodFillRecursion.Models;
+
+//------------------------------------------------------------
+// Start of the Main Method
+//------------------------------------------------------------
+
+// Declare and initialize
+// Create a new BoardModel
+BoardModel board = new BoardModel(20, 3);
+
+// Print the board to the console
+Utility.PrintBoard(board);
+
+//------------------------------------------------------------
+// End of the Main Method
+//------------------------------------------------------------
+
+static class Utility
+{
+    /// <summary>
+    /// Print the board to the console
+    /// </summary>
+    /// <param name="board"></param>
+    public static void PrintBoard(BoardModel board)
+    {
+        // Make sure the color of the column numbers is white
+        Console.ForegroundColor = ConsoleColor.White;
+
+        // Start the column numbers row with a space to keep the numbers aligned
+        Console.Write(" ");
+
+        // Loop to add column numbers for the board
+        for (int colNum = 0; colNum < board.Size; colNum++)
+        {
+            // Print the colNum with a 2-character width
+            Console.Write($"{colNum + 1,3}");
+        }
+
+        Console.WriteLine();
+
+        // Loop through the rows of the board
+        for (int row = 0; row < board.Size; row++)
+        {
+            // Print each row number in white
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"{row + 1,2}");
+
+            // Loop through the columns of the board
+            for (int col = 0; col < board.Size; col++)
+            {
+                // Check if the current cell is a wall
+                if (board.Grid[row, col].Contents == "W")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(" W ");
+                }
+                else if (board.Grid[row, col].Contents == "E")
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(" . ");
+                }
+                else
+                {
+                    Console.Write("   ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    } // End of PrintBoard method
+}
