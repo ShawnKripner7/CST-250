4aa7599c6af0fb722a344d10c199ae0c0b344349
ShawnKripner7 <shawn.kripner7@gmail.com>
2026-04-17 19:18:43 -0700

    Add logic to send the pizzas to a text file.

--- Activity 4/FrmOrderDetails.Designer.cs
+++ Activity 4/FrmOrderDetails.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PizzaMaker
 {
-    partial class FrmOrderSummary
+    partial class FrmOrderDetails
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmOrderSummary";
+            lblOrderDetails = new Label();
+            btnSaveOrder = new Button();
+            SuspendLayout();
+            // 
+            // lblOrderDetails
+            // 
+            lblOrderDetails.AutoSize = true;
+            lblOrderDetails.Location = new Point(46, 49);
+            lblOrderDetails.Name = "lblOrderDetails";
+            lblOrderDetails.Size = new Size(75, 15);
+            lblOrderDetails.TabIndex = 0;
+            lblOrderDetails.Text = "Order Details";
+            // 
+            // btnSaveOrder
+            // 
+            btnSaveOrder.Location = new Point(566, 346);
+            btnSaveOrder.Name = "btnSaveOrder";
+            btnSaveOrder.Size = new Size(75, 23);
+            btnSaveOrder.TabIndex = 1;
+            btnSaveOrder.Text = "Save Order";
+            btnSaveOrder.UseVisualStyleBackColor = true;
+            btnSaveOrder.Click += this.BtnSaveOrderClickEH;
+            // 
+            // FrmOrderDetails
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSaveOrder);
+            Controls.Add(lblOrderDetails);
+            Name = "FrmOrderDetails";
+            Text = "Pizza Order Details";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblOrderDetails;
+        private Button btnSaveOrder;
     }
 }
--- Activity 4/FrmOrderDetails.cs
+++ Activity 4/FrmOrderDetails.cs
@@ -1,18 +1,91 @@
-﻿using System;
+﻿/*
+ * Shawn Kripner
+ * CST-250
+ * 4/16/2025
+ * Pizza Maker
+ * Activity 4
+ */
+
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
+using PizzaMakerClassLibrary.Models;
+using PizzaMakerClassLibrary.Services.BusinessLogicLayer;
 
 namespace PizzaMaker
 {
-    public partial class FrmOrderSummary : Form
+    public partial class FrmOrderDetails : Form
     {
-        public FrmOrderSummary()
+        // Declare class level variables
+        private List<PizzaModel> _pizzaOrder;
+        private PizzaLogic _pizzaLogic;
+
+        public FrmOrderDetails()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Parameterized constructor for FrmOrderDetails
+        /// </summary>
+        /// <param name="pizzaOrder"></param>
+        public FrmOrderDetails(List<PizzaModel> pizzaOrderList, PizzaLogic pizzaBusinessLogic)
+        {
+            // Initialize the form
+            InitializeComponent();
+
+            // Initialize the class level variables
+            _pizzaOrder = pizzaOrderList;
+            _pizzaLogic = pizzaBusinessLogic;
+        }
+
+        /// <summary>
+        /// Display the pizzas on the form
+        /// </summary>
+        public void DisplayPizzas()
+        {
+            // Clear the label
+            lblOrderDetails.Text = "";
+
+            // Loop through the pizza order list
+            foreach (PizzaModel pizza in _pizzaOrder)
+            {
+                lblOrderDetails.Text +=
+                    $"Name: {pizza.ClientName}\n" +
+                    $"Ingredients: {string.Join(", ", pizza.Ingredients)}\n" +
+                    $"Strange Add Ons: {string.Join(", ", pizza.StrangeAddOns)}\n" +
+                    $"Crust: {pizza.Crust}\n" +
+                    $"Sauce: {pizza.SauceQty}\n" +
+                    $"Cheese: {pizza.CheeseQty}\n" +
+                    $"Delivery Time: {pizza.DeliveryTime}\n" +
+                    $"Pizza Box Color: {pizza.PizzaBoxColor}\n" +
+                    $"Price: {pizza.Price}\n\n";
+            }
+        }
+
+        /// <summary>
+        /// Click event handler for btnSaveOrder
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSaveOrderClickEH(object sender, EventArgs e)
+        {
+            // Declare and initialize
+            bool isSaveSuccess = false;
+
+            // Write the order to the file
+            isSaveSuccess = _pizzaLogic.WriteOrderToFile();
+
+            // Check if the save was successful
+            if (isSaveSuccess)
+            {
+                // Show a success message to the user
+                MessageBox.Show("The pizza order was saved.");
+            }
+            else
+            {
+                // Show a failure message to the user
+                MessageBox.Show("An error occurred while trying to save your order. Please try again later.");
+            }
         }
     }
 }
