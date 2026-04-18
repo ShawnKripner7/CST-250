d7b86aa97ab47f52644df51cf42023e229740852
ShawnKripner7 <shawn.kripner7@gmail.com>
2026-04-17 19:02:26 -0700

    : Add and complete the UI for a second form to show the current pizza order.

---
+++ Activity 4/FrmOrderSummary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PizzaMaker
+{
+    public partial class FrmOrderSummary : Form
+    {
+        public FrmOrderSummary()
+        {
+            InitializeComponent();
+        }
+    }
+}
