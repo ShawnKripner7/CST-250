30c8032d0b4c6373b66911bc17ee8d70675a16df
ShawnKripner7 <shawn.kripner7@gmail.com>
2026-04-30 21:59:42 -0700

    Create a VerseDAO and add logic to add a new verse.

---
+++ Activity 6/Services/DataAccessLayer/VerseDAO.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Shawn Kripner
+ * CST-250
+ * 4/30/2026
+ * File I/O and LINQ
+ * Activity 6
+ */
+
+using FileIOAndLINQ.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileIOAndLINQ.Services.DataAccessLayer
+{
+    class VerseDAO
+    {
+        // Declare class level variables
+        List<VerseDataModel> _verses;
+
+        /// <summary>
+        /// Default constructor for VerseDAO
+        /// </summary>
+        public VerseDAO()
+        {
+            // Create a new List of VerseDataModels
+            _verses = new List<VerseDataModel>();
+        }
+
+        /// <summary>
+        /// Add a new verse to the inventory
+        /// </summary>
+        /// <param name="verse"></param>
+        /// <returns></returns>
+        public int AddVerse(VerseRequestModel verse)
+        {
+            int id = _verses.Count + 1;
+            VerseDataModel newVerse = new VerseDataModel();
+
+            newVerse = new VerseDataModel(id, verse.Book, verse.Chapter,
+                verse.Verse, verse.Text, verse.Meaning, verse.Importance);
+
+            _verses.Add(newVerse);
+
+            return id;
+        }
+    }
+}
